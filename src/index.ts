ca354335c3c7c551a8a68cc4b47e3da96706ed57
Zenith Wogwugwu <uzenith360@gmail.com>
2023-01-20 03:39:23 +0100

    fix: add jwtTokenTypeEnum export

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { JwtUtils } from "./jwt-utils";
 
 export { JwtUtils } from "./jwt-utils";
 export * as JwtPayloadInterface from './jwt.interface';
+export * as jwtTokenTypeEnum from './jwt-token-type.enum';
 
 export default {
     JwtUtils,
