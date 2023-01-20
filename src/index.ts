502a6071f6497e871b48c5ba3ac328b70afcef8f
Zenith Wogwugwu <uzenith360@gmail.com>
2023-01-20 03:42:45 +0100

    fix: correct JwtInterface name in exports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { JwtUtils } from "./jwt-utils";
 
 export { JwtUtils } from "./jwt-utils";
-export * as JwtPayloadInterface from './jwt.interface';
+export * as JwtInterface from './jwt.interface';
 export * as jwtTokenTypeEnum from './jwt-token-type.enum';
 
 export default {
